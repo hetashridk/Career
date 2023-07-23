015cc5da8273cb31015db1372bfa3d723a97dcb7
hetashridk <hetashrik@gmail.com>
2023-07-23 22:51:27 +0530

    pages folder added

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import Contact from "@/components/Layout/Contact";
+import Nav from "@/components/Layout/Nav";
+
+
+
+
+function Index() {
+  return (
+    <>
+      <Nav />
+      {/* <Contact /> */}
+    </>
+  
+  );
+}
+
+export default Index;
